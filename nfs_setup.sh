08035520466f260f65af2b133b6d51fe12a7507e
Lucas Russo <lerwys@gmail.com>
2018-04-02 10:24:41 -0300

    nfs_setup.sh: add insecure option to NFS options

--- nfs_setup.sh
+++ nfs_setup.sh
@@ -9,7 +9,7 @@ echo "#NFS Exports" > /etc/exports
 for mnt in "${mounts[@]}"; do
   src=$(echo $mnt | awk -F':' '{ print $1 }')
   mkdir -p $src
-  echo "$src *(ro,async,no_subtree_check,no_root_squash)" >> /etc/exports
+  echo "$src *(ro,async,no_subtree_check,no_root_squash,insecure)" >> /etc/exports
 done
 
 exec runsvdir /etc/sv
