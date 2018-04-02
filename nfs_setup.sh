1cdcf15bde3012036e6cf619affc2308fbb929bd
Lucas Russo <lerwys@gmail.com>
2018-04-02 17:55:42 -0300

    nfs_setup.sh: don't expand inputs, as "*" would result in error
    The character "*" would result in the current
    directory contents, instead of the actual
    "*" character.

--- nfs_setup.sh
+++ nfs_setup.sh
@@ -9,9 +9,11 @@ echo "#NFS Exports" > /etc/exports
 for mnt in "${mounts[@]}"; do
   OLDIFS=$IFS; IFS=',';
   # Separate "tuple" arguments with positional notation
+  set -o noglob
   set -- ${mnt};
   path=$1
   net=$2
+  set +o noglob
 
   src=$(echo $path | awk -F':' '{ print $1 }')
   mkdir -p $src
